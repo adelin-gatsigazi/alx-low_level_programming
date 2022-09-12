f2213180f0cfa33021d37b65eb6509690f9ce207
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2022-09-13 01:25:10 +0200

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function 
+*
+*Description: using the main function 
+* this program prints "programming is like building a multingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar,but the outcome is a piece of art,\n")
+Return (0);
+}
