f59801dc3772fd091ade9cabd1269ed3c63b4ffe
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2022-09-13 01:38:27 +0200

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function 
+* main - print the string in the put function
 *
-*Description: using the main function 
-* this program prints "programming is like building a multingual puzzle
+* Description: using the main function
+* this program prints "programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
-printf("with proper grammar,but the outcome is a piece of art,\n")
-Return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
