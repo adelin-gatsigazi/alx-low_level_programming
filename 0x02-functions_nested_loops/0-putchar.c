a6ddb784ee3d4d87fca46857b9bdfbb13c8c69b5
Adelin <59762310+adelin-gatsigazi@users.noreply.github.com>
2022-09-14 01:46:49 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,6 @@
 
 int main(void)
 {
-        0-putchar('_');
-        0-putchar('p');
-        0-putchar('u');
-        0-putchar('t');
-        0-putchar('c');
-        0-putchar('h');
-        0-putchar('a');
         _putchar('_');
         _putchar('p');
         _putchar('u');
@@ -25,3 +18,6 @@ int main(void)
         _putchar('\n');
 
         return (0);
+        
+}
+        
