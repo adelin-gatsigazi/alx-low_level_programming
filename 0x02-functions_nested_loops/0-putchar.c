8677c00d0ccecfa5c3372f0112304e75df3e4d0e
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2022-09-14 00:38:04 +0200

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+        0-putchar('_');
+        0-putchar('p');
+        0-putchar('u');
+        0-putchar('t');
+        0-putchar('c');
+        0-putchar('h');
+        0-putchar('a');
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+
+        return (0);
