fef63dece95b10618812eebd4dbe4313f15828c3
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2022-09-14 17:48:35 +0200

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10  -> prints the lowercase alphabets
+*/
+
+void print_alphabet_x10(void)
+{
+int j;
+int x;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
