73251259aef35a5e7ca8bbfbd8c5791b01f8fcce
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2022-09-14 13:50:06 +0200

    error fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** main - prints putchar
+/* main - prints putchar
 * Return: always 0
 */
 int main(void)
@@ -14,8 +14,8 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-    
-return (0);
+ 
+ return (0);
     
 }
 
