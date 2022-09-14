f1efda71bc16a43f92bf6c60bc0cda6fa0631732
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2022-09-14 20:40:01 +0200

    checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
 * _islower -> checks if the character is lowercase
 * Return: returns 1and 0 depending on condition
 */
-int islower(int c)
+int _islower(int c)
 {
 return (c >= 'a' && c <= 'z');
 }
