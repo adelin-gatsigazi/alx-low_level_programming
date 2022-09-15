c8262b9237478e1d841714e6974e1a7b876605e7
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2022-09-16 01:24:14 +0200

    prints 10 times the numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,7 +6,6 @@
 void more_numbers(void)
 {
 int x, y;
-
 for (x = 0; x < 10; x++)
 {
 for (y = 0; y < 15; y++)
@@ -14,4 +13,5 @@ for (y = 0; y < 15; y++)
 _putchar(y + '0');
 }
 _putchar('\n');
-}}
+}
+}
