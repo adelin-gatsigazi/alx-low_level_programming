855910b95fffb3e79e0b2d1a74bb8049683487c1
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2022-09-16 16:28:15 +0200

    golden buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,13 +5,13 @@
  * main - prints the numbers from 1 to 100, followed by a new line
  * yet for multiples of three prints Fizz instead of the number
  * and for the multiples of five prints Buzz
- * Return: Always 0
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 int b;
 
-for (b = 1; i <= 100; b++)
+for (b = 1; b <= 100; b++)
 {
 if (b % 3 == 0 && b % 5 != 0)
 {
