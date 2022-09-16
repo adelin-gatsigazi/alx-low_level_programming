4b2b105fda5a2b7dceea96d4fd91a0d7e9615648
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2022-09-16 15:30:03 +0200

    gold prints a square,

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+/**
+* print_diagonal -> printing line
+* @n: integer params
+*/
+void print_square(int size)
+{
+int a, b;
+
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++)
+{
+_putchar(35);
+}
+_putchar('\n);
+}
+_putchar('\n');
+}
+}
