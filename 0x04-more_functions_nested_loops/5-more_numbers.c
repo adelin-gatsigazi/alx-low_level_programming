e336dd76d760801e95ba5bac8c4ca348415595af
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2022-09-16 02:44:49 +0200

    prints 10 times the numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,17 +2,18 @@
 
 /**
 * more_numbers -> printing more numbers
+*
+* Return: Always 0.
 */
 void more_numbers(void)
 {
 int x, y;
 
-for (x = 0; x < 10; x++)
+for ( x = 0; x < 10; x++)
 {
 for (y = 0; y < 15; y++)
 {
-_putchar((y + '0');
+_putchar(y + '0');
 }
 _putchar('\n');
-}
-}
+}}
