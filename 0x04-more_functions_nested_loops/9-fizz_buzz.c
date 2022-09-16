0d3f4343891ce0a91205b465e175eafae5b6d50e
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2022-09-16 16:24:01 +0200

    golden buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,32 +5,36 @@
  * main - prints the numbers from 1 to 100, followed by a new line
  * yet for multiples of three prints Fizz instead of the number
  * and for the multiples of five prints Buzz
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(void)
 {
-	int b;
+int b;
 
-	for (b = 1; i <= 100; b++)
-	{
-		if (b % 3 == 0 && b % 5 != 0)
-		{
-			printf(" Fizz");
-		} else if (b % 5 == 0 && b % 3 != 0)
-		{
-			printf(" Buzz");
-		} else if (b % 3 == 0 && b % 5 == 0)
-		{
-			printf(" FizzBuzz");
-		} else if (b == 1)
-		{
-			printf("%d", b);
-		} else
-		{
-			printf(" %d", b);
-		}
-	}
-	printf("\n");
+for (b = 1; i <= 100; b++)
+{
+if (b % 3 == 0 && b % 5 != 0)
+{
+printf(" Fizz");
+}
+else if (b % 5 == 0 && b % 3 != 0)
+{
+printf(" Buzz");
+}
+else if (b % 3 == 0 && b % 5 == 0)
+{
+printf(" FizzBuzz");
+}
+else if (b == 1)
+{
+printf("%d", b);
+}
+else
+{
+printf(" %d", b);
+}
+}
+printf("\n");
 
-	return (0);
+return (0);
 }
