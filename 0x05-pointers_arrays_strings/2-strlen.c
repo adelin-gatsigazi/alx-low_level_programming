11cd7a42afbf62daabec268062470d47c522f04f
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2022-09-20 04:21:53 +0200

    gold pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,17 @@
 #include "main.h"
-
+#include <string.h>
 /**
-* _strlen - returns the length of a string
-* @s: string
-* Return: returns length as integer;
+* _strlen -> function to get the length of a string
+* @s: string pointer to passed to this function
+* Return: returns length of the string
 */
 int _strlen(char *s)
 {
-	int len = 0;
+	int len;
 
-	while (*(s + len) != '\0')
-              len++;
-
+	for(; *s != '\0'; s++)
+	{
+		len += 1;
+	}
 	return (len);
 }
