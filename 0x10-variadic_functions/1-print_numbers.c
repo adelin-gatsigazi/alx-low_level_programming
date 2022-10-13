03ba96f45433cdfd44300b5d4a20f2900f49e111
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2022-10-14 01:13:09 +0200

    Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,32 +1,29 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
-*print_numbers - prints numbers followed by
-*new line
-*@separator: string to be printed between numbers
-*@n: number of integer to be passed
-*
-*Return: nothing
-*/
-
+ * print_numbers - Prints numbers, followed by a new line.
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list nums;
+	unsigned int index;
 
-va_list li;
-unsigned int i;
+	va_start(nums, n);
 
-va_start(li, n);
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(nums, int));
 
-for (i = 0; i < n; i++)
-{
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
 
+	printf("\n");
 
-printf("%d", va_arg(li, int));
-
-if (i != (n - 1) && separator != NULL)
-printf("%s", separator);
-}
-printf("\n");
-va_end(li);
-
+	va_end(nums);
 }
